0dad2ee30148268e91a9e02c31a98dff92fc5d90
Nilson Sasaki <nilsonsasaki@gmail.com>
2023-02-15 09:41:20 -0300

    feat: remove hello endpoint

--- src/main/java/io/github/nilsonsasaki/SalesApplication.java
+++ src/main/java/io/github/nilsonsasaki/SalesApplication.java
@@ -2,17 +2,9 @@ package io.github.nilsonsasaki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SalesApplication {
-
-    @GetMapping("/hello")
-    public String helloWorld(){
-        return "hello world!";
-    }
 
     public static void main(String[] args) {
 
